d60f2d034002a5b551905687490016dcf8a74d10
lauralardies <lrodrlar@myuax.com>
2022-10-24 10:45:33 +0200

    calse vikingo

--- vikingsclasses.py
+++ vikingsclasses.py
@@ -11,3 +11,20 @@ class Soldier():
     
     def receiveDamage(self, damage):
         self.health = self.health - damage
+
+class Viking():
+    def __init__(self, name, health, strength) -> None:
+        Soldier.__init__(self, health, strength)
+        self.name = name
+    
+    Soldier.attack()
+
+    def receiveDamage(self, damage):
+        self.health = self.health - damage
+        if self.health <= 0:
+            return (self.name, ' has received ', damage, ' points of damage')
+        else:
+            return (self.name, ' has died in act of combat')
+    
+    def battleCry(self):
+        return ('Odin Owns You All!')

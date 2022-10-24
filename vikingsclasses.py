b2cd9fc82f6f8c448961fe1265c314bec36f7e11
lauralardies <lrodrlar@myuax.com>
2022-10-24 11:24:14 +0200

    clase war creada

--- vikingsclasses.py
+++ vikingsclasses.py
@@ -45,3 +45,34 @@ class Saxon(Soldier):
         else:
             sol = 'A Saxon has died in combat'
             return sol
+
+class War():
+    def __init__(self) -> None:
+        self.vikingArmy = []
+        self.saxonArmy = []
+
+    def addViking(self, viking):
+        self.vikingArmy.append(viking)
+    
+    def addSaxon(self, saxon):
+        self.saxonArmy.append(saxon)
+    
+    def vikingAttack(self):
+        Saxon.receiveDamage(self, Viking.self.strength)
+        if Saxon.self.health <= 0:
+            self.saxonArmy.remove(Saxon)
+        return (Saxon.receiveDamage(self, Viking.self.strength), Viking.self.strength)
+    
+    def saxonAttack(self):
+        Viking.receiveDamage(self, Saxon.self.strength)
+        if Viking.self.health <= 0:
+            self.vikingArmy.remove(Viking)
+        return (Viking.receiveDamage(self, Saxon.self.strength), Saxon.self.strength)
+    
+    def showStatus(self):
+        if len(self.saxonArmy) == 0:
+            return 'Vikings have won the war of the century!'
+        elif len(self.vikingArmy) == 0:
+            return 'Saxons have fought for their lives and survive another day...'
+        else:
+            return 'Vikings and Saxons are still in the thick of battle.'
